aed7d0df3ac86ff8523b09a8698e160a9643f7e9
196code-gray <doco827@gmail.com>
2024-05-11 13:43:13 +0900

    Fix: "수정 주문 정률 할인"
    VIP는 금액에서 1000원 할인이 아닌 10% 정률 할인으로 변경됨.

--- src/main/java/com/spring/basic/order/OrderServiceImpl.java
+++ src/main/java/com/spring/basic/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package com.spring.basic.order;
 
 import com.spring.basic.discount.DiscountPolicy;
 import com.spring.basic.discount.FixDiscountPolicy;
+import com.spring.basic.discount.RateDiscountPolicy;
 import com.spring.basic.member.Member;
 import com.spring.basic.member.MemberRepository;
 import com.spring.basic.member.MemoryMemberRepository;
@@ -10,7 +11,14 @@ import com.spring.basic.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    // 고정 할인에서 정률 할인으로 변경
+    /*
+    의존 관계이기 때문에 고정 할인을 의존하고 있던 OrderServiceImpl의 로직을 변경해야 함.
+    DIP원칙을 지키려면 아래와 같이 구현체를 없애면 됨.
+    하지만 말이 안됨.
+    그럼 어떻게 지킬 수 있는가?
+     */
+    private DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
