d4ce9c4b44c599cf6c04df2bef622b6c0282d8c5
196code-gray <doco827@gmail.com>
2024-05-14 10:58:56 +0900

    Feat: "추가 xml bean 추가&조회 테스트"
    xml 코드로 빈을 등록하고 조회하는 테스트.

---
+++ src/test/java/com/spring/basic/xml/XmlAppContex.java
@@ -0,0 +1,19 @@
+package com.spring.basic.xml;
+
+import com.spring.basic.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class XmlAppContex {
+
+    @Test
+    @DisplayName("xml로 빈 등록하기 성공")
+    void xmlAppContext(){
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
