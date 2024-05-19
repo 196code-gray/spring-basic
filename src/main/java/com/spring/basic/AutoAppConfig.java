8974c4a1130fad256b1ac95a02d742cc5541fde3
196code-gray <doco827@gmail.com>
2024-05-19 12:26:13 +0900

    Refactor: "수정 수동 bean 등록"
    자동으로 등록한 bean과 수동으로 등록한 같은 이름의 bean이 있을때 어떻게 되는지 확인.

--- src/main/java/com/spring/basic/AutoAppConfig.java
+++ src/main/java/com/spring/basic/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package com.spring.basic;
 
+import com.spring.basic.member.MemberRepository;
+import com.spring.basic.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -12,4 +15,17 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
+/*
+수동 등록 vs 자동 등록
+수동등록이 우선권을 가짐.
+그래서 이름이 같은 빈이여도 수동 등록한 것이 빈으로 등록이 됨.
+하지만 이런 수동 등록의 경우 여러 개발자가 개발을 진행하면서 꼬일 확률 높음.
+spring boot에서는 이렇게 이름이 같은 빈이 있으면 무조건 에러를 발생시키게 변경.
+
+애매한 상황 만들지 않기(명확하지 않은 것 xx). 까먹음. 혼자개발하는 것 아님.
+ */
+    @Bean("memoryMemberRepository")
+    MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
 }
